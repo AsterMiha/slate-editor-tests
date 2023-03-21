89f6eb0d7a1cdd6c9beb9128b8d41342bc6053ef
Mihaela Chirea <chireamihaela99@gmail.com>
2023-03-21 10:46:27 +0100

    Add empty node

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,222 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Import React dependencies.
+import React, { useState, useCallback } from "react";
+// Import the Slate editor factory.
+import {
+  createEditor,
+  Descendant,
+  Transforms,
+  Editor,
+} from "slate";
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact, RenderElementProps } from "slate-react";
+import { Exercise, Question, Solution } from "./react-app-env";
+
+const initialValue: Descendant[] = [
+  {
+    type: "exercise",
+    children: [
+      {
+        type: "question",
+        children: [
+          {
+            type: "paragraph",
+            children: [
+              { type: "text", text: "What is the capital of Germany?" },
+            ],
+          },
+        ],
+      },
+      {
+        type: "solution",
+        children: [
+          {
+            type: "paragraph",
+            children: [{ type: "text", text: "Paragraph 1" }],
+          },
+          {
+            type: "paragraph",
+            children: [{ type: "text", text: "Paragraph 2" }],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "exercise",
+    children: [
+      {
+        type: "question",
+        children: [
+          {
+            type: "paragraph",
+            children: [{ type: "text", text: "Question2?" }],
+          },
+        ],
+      },
+      {
+        type: "solution",
+        children: [
+          {
+            type: "paragraph",
+            children: [{ type: "text", text: "Paragraph 1" }],
+          },
+          {
+            type: "paragraph",
+            children: [{ type: "text", text: "Paragraph 2" }],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const emptyQ: Question = {
+  type: "question",
+  children: [
+    {
+      type: "paragraph",
+      children: [{ type: "text", text: "Question text?" }],
+    },
+  ],
+};
+
+const emptyS: Solution = {
+  type: "solution",
+  children: [
+    {
+      type: "paragraph",
+      children: [{ type: "text", text: "Solution text" }],
+    },
+  ],
+};
 
 function App() {
+  const [editor] = useState(() =>
+    withCustomNormalization(withReact(createEditor()))
+  );
+
+  const renderElement = useCallback((props: RenderElementProps) => {
+    switch (props.element.type) {
+      case "question":
+        return (
+          <div
+            style={{
+              border: "orange solid 1px",
+              borderRadius: "2px",
+              padding: "0.3em",
+              marginBottom: "0.2em"
+            }}
+            {...props.attributes}
+          >
+            {" "}
+            {props.children}{" "}
+          </div>
+        );
+      case "exercise":
+        return (
+          <div
+            style={{
+              border: "blue solid 1px",
+              borderRadius: "2px",
+              padding: "0.3em",
+            }}
+            {...props.attributes}
+          >
+            {" "}
+            {props.children}{" "}
+          </div>
+        );
+      case "code":
+        return (
+          <pre {...props.attributes}>
+            <code>{props.children}</code>
+          </pre>
+        );
+      case "paragraph":
+        return <p {...props.attributes}> {props.children} </p>;
+      case "solution":
+        return (
+          <div
+            style={{
+              border: "green solid 1px",
+              borderRadius: "2px",
+              padding: "0.3em",
+            }}
+            {...props.attributes}
+          >
+            {" "}
+            {props.children}{" "}
+          </div>
+        );
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: "2em"}}>
+    <Slate editor={editor} value={initialValue}>
+      <Editable renderElement={renderElement} />
+    </Slate>
     </div>
   );
+}
+
+function addMissing(exercise: Exercise): Exercise {
+  const question: Question = emptyQ;
+  const solution: Solution = emptyS;
+
+  if (exercise.children.length === 2)
+    return exercise;
+
+  let newExercise: Exercise = {
+    type: 'exercise',
+    children: [question, solution],
+  }
+
+  console.log(exercise.children);
+  if (question.children.length === 0) {
+    newExercise.children[0] = emptyQ;
+  }
+
+  if (solution.children.length === 0) {
+    newExercise.children[1] = emptyS;
+  }
+
+  return newExercise;
+}
+
+function withCustomNormalization(editor: Editor) {
+  let { normalizeNode } = editor;
+
+  editor.normalizeNode = ([entry, path]) => {
+    if (path.length === 0) {
+      console.log(editor.children.length);
+
+      console.log("___________");
+      // Check that all elements are exercises
+      for (let i = 0; i < editor.children.length; i++) {
+        console.log("- " + i + ": " + editor.children[i].type);
+        const element = editor.children[i];
+        if (element.type === "exercise" && element.children.length !== 2) {
+          console.log(element.children.length);
+          // Check exercise composition
+          const newEx: Exercise = addMissing(element);
+          // Transforms.setNodes(editor, newEx, {at: [i]});
+          Transforms.removeNodes(editor, {at: [i]});
+          Transforms.insertNodes(editor, newEx, {at: [i], select: true});
+
+        } else {
+          // Remove elements that are not exercises
+        }
+      }
+      console.log("___________");
+    }
+
+    // Fall back to the original `normalizeNode` to enforce other constraints.
+    return normalizeNode([entry, path]);
+  };
+  return editor;
 }
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,75 @@
 /// <reference types="react-scripts" />
+
+// TypeScript users only add this code
+import { BaseEditor } from "slate";
+import { ReactEditor } from "slate-react";
+
+type CustomElement = Paragraph | Code | Question | Solution | Exercise;
+type ExercisePart = Question | QuestionBody | Solution | SolutionBody;
+type CustomText = { type: "text"; text: string };
+
+interface Paragraph {
+  type: "paragraph";
+  children: CustomText[];
+}
+interface Code {
+  type: "code";
+  children: CustomText[];
+}
+
+interface Exercise {
+  type: "exercise";
+  // children: [Question, Solution];
+  children: (Question | Solution)[];
+}
+
+// { "type": "exercise", "question": ..., "solutio": ... } -> { type: "exercise", children: [] }
+// Dict <-> List of tuples
+
+
+const example: Exercise = {
+  type: "exercise",
+  children: [
+    {
+      type: "question",
+      children: [
+        {
+          type: "paragraph",
+          children: [{ text: "What is the captial of Germany?" }],
+        },
+      ],
+    },
+    {
+      type: "solution",
+      children: [
+        { type: "paragraph", children: [{ text: "Paragraph 1" }] },
+        { type: "paragraph", children: [{ text: "Paragraph 1" }] },
+      ],
+    },
+  ],
+};
+
+/*
+Question title - 1 line
+Question body - variable length
+Solution title - 1 line
+Solution body - variable length
+*/
+interface Question {
+  type: "question";
+  children: Paragraph[];
+}
+//interface QuestionBody { type: 'questionBody'; children: CustomText[] }
+interface Solution {
+  type: "solution";
+  children: Paragraph[];
+}
+//interface SolutionBody { type: 'solutionBody'; children: CustomText[] }
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
